52904bb6c22bc7afb62dd229e5cd5f19a8410429
Nadya <nadejda.skillup@gmail.com>
2020-10-06 21:03:35 +0300

    'js7'

--- js-task_7/index.js
+++ js-task_7/index.js
@@ -1,15 +1,18 @@
 const c = 4.19; // теплоемкость воды
 
 let Teapot = function(name) {
+    this.name=name;
+    this.power=0;
+    this.volume=0;
+    this.turn = false;
+
     this.get = function() {
-        this.name = name;
         this.power = +prompt("Введите мощность, Вт");
-        this.volume = +prompt("Введите объем чайника, мл"); 
-        this.turn = false;
+        this.volume = +prompt("Введите объем чайника, мл");        
     }
 
     function calcTime(power, v) {
-        return (v*c)/power;  //не уверена в правильности формулы =)
+        return Math.round((v*c)/power);  
     }
     function checkVolume(min, max) {
         return (min>=max) ? false : true;
@@ -17,16 +20,17 @@ let Teapot = function(name) {
     this.start = function() {      
         let waterVolume = +prompt("Какой объем воды заливаете в чайник? мл");  
         if (checkVolume(waterVolume, this.volume)) {
-            alert("Чайник закипит через "+calcTime(this.power, waterVolume) +'минуты');
+            alert("Чайник закипит через "+calcTime(this.power, waterVolume) +' минуты');
             this.turn=true;
         } else {
             alert("Объем чайника меньше заливаемого объема воды");
         }
     }
-    // function showInfo() {
-            //как правильно передать данные именно во внутреннюю функцию для отображения информации?
-    // }
+    function showInfo() {
+        let txt="Чайник "+this.name+"<br>"; //не работает вот такой вызов
+    }
     this.show = function() {
+        console.log(showInfo());
         document.write("Чайник "+this.name+"<br>");
         document.write("Мощность "+this.power+"<br>");
         document.write("Объем "+this.volume+"<br>");
