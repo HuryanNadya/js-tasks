0a233d62405c66208ae189617d17dbf6b7906c95
Nadya <nadejda.skillup@gmail.com>
2020-10-13 19:17:37 +0300

    'js9'

--- js-task_9/js/script.js
+++ js-task_9/js/script.js
@@ -2,59 +2,83 @@
    
     let listInput = doc.querySelector("#todo-input");     
     let listInputValue;
-
         
-    let list = doc.querySelector(".todo-list");
-   
-
-
-    listInput.addEventListener("keyup", function(event){
-        listInputValue = listInput.value;
-        if (event.keyCode==13) {
-            (checkInput()) ? createList(listInputValue) : "";            
-        }
-    });
-
-    //создание нового пункта
-    let createList = function(value) {
-            
-        list.insertAdjacentHTML('beforeEnd', `<li>
-        <div class="form-check"> 
-            <label class="form-check-label"> 
-                <input class="checkbox" type="checkbox">${value}<i class="input-helper"></i>
-            </label> 
-        </div> 
-        <i class="remove"></i>
-        </li>            
-        
-        
-        `);
-        listInput.value="";       
-    }
+    let list = doc.querySelector(".todo-list"); 
 
     //проверка input
-    let checkInput = function() {        
+    let checkInput = function() {     
+        listInputValue = listInput.value;   
         if (listInputValue.length==0 || listInputValue==" ") {  //доделать проверку
-            alert("Введите не пустое значение" );
-            return false;
+            alert("You must write something!" );
+            return;
         } else {
-            return true;
+            createList(listInputValue);      
+            
         }
     }
+    //создание нового пункта
+    let createList = function(value) {
+        list.insertAdjacentHTML('afterBegin', `<li>
+        <div class="form-check"> 
+            <label class="form-check-label"> 
+                <input class="checkbox" type="checkbox"><span>${value}</span><i class="input-helper"></i>                
+            </label> 
+        </div> 
+        <div class="buttons">
+            <i class="edit"></i>
+            <i class="remove"></i>
+        </div>
+        </li>`);
+        listInput.value="";
+        setTimeout(function() {                    
+            list.firstElementChild.classList.add("fade-in");
+          }, 200);
+        
+        removeList(); //срабатывает несколько раз, если есть несколько пунктов списка. Как правильно убрать эту ошибку?
+        editList();   
+    }
 
-    let listItem=list.querySelectorAll(".form-check-label");
-    for (let i=0; i<listItem.length; i++) {
-        listItem[i].addEventListener("click", function() {
-            let parent = this.closest("li");          
-            parent.classList.toggle("completed");
+    //удаление элемента списка
+    let removeList = function() {
+        let removeButton= doc.querySelectorAll(".remove") ; 
+        removeButton.forEach(function(element) {           
+            element.addEventListener("click", function() {
+                this.closest("li").classList.add("fade-out");
+                setTimeout(function() {                    
+                    element.closest("li").remove();
+                }, 500);               
+                
+            });
+                    
+        });
+    };
+
+
+    //редактирование нового пункта
+    let editList = function() {
+        let editButton= doc.querySelectorAll(".edit"); 
+        editButton.forEach(function(element) {        
+            element.addEventListener("click", function() {
+                let parent=this.closest("li");
+                let parentSpan=parent.querySelector(".form-check-label span");
+                let oldValue=parentSpan.textContent;                
+                let newValue = prompt("Редактирование заметки", oldValue);
+                if (newValue) {
+                    parentSpan.textContent=newValue;
+                }   
+            } );        
         })
     }
-    // listItem.forEach(function(element) {
-    //     element.addEventListener("click", function() {
-    //         console.log(this);
-    //     })
-       
-    // })
+
+    
+    //проверка на ввод Enter
+    listInput.addEventListener("keyup", function(event){        
+        if (event.keyCode==13) {
+            checkInput()          
+        }
+    });
+    doc.querySelector("#add-button").addEventListener("click", checkInput);   
+
 
 
   
